5024106abb95ab7e4cee6c2d5bffaef864c6a509
Javad Sabbagh <javad.sabbaq@gmail.com>
2019-12-07 19:27:32 +0330

    Add transition

--- vuejs-training/src/components/HelloWorld.vue
+++ vuejs-training/src/components/HelloWorld.vue
@@ -11,6 +11,17 @@
       placeholder="Pick a day">
     </el-date-picker>
 
+    <el-button @click="show = !show">Click Me</el-button>
+
+    <div style="display: flex; margin-top: 20px; height: 100px;">
+      <transition name="el-fade-in-linear">
+        <div v-show="show" class="transition-box">.el-fade-in-linear</div>
+      </transition>
+      <transition name="el-fade-in">
+        <div v-show="show" class="transition-box">.el-fade-in</div>
+      </transition>
+    </div>
+
   </div>
 </template>
 
@@ -18,7 +29,10 @@
 export default {
   name: 'HelloWorld',
   data: function() {
-      return {visible: false}
+      return {
+        visible: false,
+        show: true
+      }
   },   
   props: {
     msg: String
@@ -42,4 +56,17 @@ li {
 a {
   color: #42b983;
 }
+
+.transition-box {
+    margin-bottom: 10px;
+    width: 200px;
+    height: 100px;
+    border-radius: 4px;
+    background-color: #409EFF;
+    text-align: center;
+    color: #fff;
+    padding: 40px 20px;
+    box-sizing: border-box;
+    margin-right: 20px;
+  }
 </style>
